4859e7d2952356c3ed6f4b093d81c5520cf68bc4
Victor Faust <vfaust.thefuture@gmail.com>
2023-03-14 01:34:52 +0300

    woah

--- net/main.cpp
+++ net/main.cpp
@@ -3,16 +3,28 @@
 # include <iostream>
 
 # include "net.hpp"
-
+std::string make_string(boost::asio::streambuf& streambuf)
+{
+	return { boost::asio::buffers_begin(streambuf.data()),
+			boost::asio::buffers_end(streambuf.data()) };
+}
 int main(void)
 {
 	net::netqueue<2> Queue(1337);
+	Queue.add(80);
 	
 	while (Queue.size() == 0)
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 
+	std::cout << "Ok" << std::endl;
 	boost::asio::ip::tcp::socket Socket(std::move(Queue.pull_one()));
-	Socket.write_some(boost::asio::buffer("Hello\0", 7));
+	std::cout << "Socket moved" << std::endl;
+	boost::asio::streambuf StreamBuffer;
+	std::cout << "SS created" << std::endl;
+	boost::asio::read_until(Socket, StreamBuffer, "\0");
+	std::cout << "read" << std::endl;
+	std::cout << make_string(StreamBuffer) << std::endl;
+	Socket.write_some(boost::asio::buffer("HTTP/1.0 200 OK\n\n<p>Hello</p>\n", 31));
 
 	while(true) { }
 }
